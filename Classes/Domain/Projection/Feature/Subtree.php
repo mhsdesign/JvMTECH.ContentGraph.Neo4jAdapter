c52f679669352bcb6766c834e2f61fa91f988a4f
Michel Loew <michel.loew@jvm.ch>
2025-09-01 17:39:08 +0200

    WIP: Satisfy Feature/08-NodeMove

--- Classes/Domain/Projection/Feature/Subtree.php
+++ Classes/Domain/Projection/Feature/Subtree.php
@@ -161,45 +161,90 @@ trait Subtree
         DimensionSpacePointSet $affectedDimensionSpacePoints,
     ): void {
         $affectedDimensionSpacePointHashes = $affectedDimensionSpacePoints->getPointHashes();
-        $this->client->runStatement(
-            Statement::create('
-            MATCH (currentNode:Node {aggregateId: $aggregateId})-[rel:IS_CHILD {contentStreamId: $contentStreamId}]->()
-            WHERE rel.dimensionSpacePointHash IN $affectedDimensionSpacePointHashes
 
-            // Get existing tags for this specific relationship and merge with new tag
-            WITH currentNode, rel,
-                 CASE WHEN rel.subtreeTags IS NOT NULL
-                      THEN apoc.convert.fromJsonMap(rel.subtreeTags)
-                      ELSE {} END as existingTags
+        foreach ($affectedDimensionSpacePointHashes as $dimensionSpacePointHash) {
+            $this->client->runStatement(
+                Statement::create('
+                // Find the node and its parent relationship to get inherited tags
+                MATCH (currentNode:Node {aggregateId: $aggregateId})-[currentRel:IS_CHILD {contentStreamId: $contentStreamId, dimensionSpacePointHash: $dimensionSpacePointHash}]->(parentNode)
 
-            // Set the new tag on the current node, preserving existing tags
-            SET rel.subtreeTags = apoc.convert.toJson(existingTags)
+                // Get parent\'s relationship to understand what tags should be inherited
+                OPTIONAL MATCH (parentNode)-[parentRel:IS_CHILD {contentStreamId: $contentStreamId, dimensionSpacePointHash: $dimensionSpacePointHash}]->()
 
-            // Handle child nodes - find all descendants
-            WITH currentNode
-            OPTIONAL MATCH (childNode)-[childRels:IS_CHILD*.. {contentStreamId: $contentStreamId}]->(currentNode)
-            WHERE all(childRel IN childRels WHERE childRel.contentStreamId = $contentStreamId
-            AND childRel.dimensionSpacePointHash IN $affectedDimensionSpacePointHashes)
+                // Get current and parent tags
+                WITH currentNode, currentRel, parentRel,
+                     CASE WHEN currentRel.subtreeTags IS NOT NULL
+                          THEN apoc.convert.fromJsonMap(currentRel.subtreeTags)
+                          ELSE {} END as currentTags,
+                     CASE WHEN parentRel IS NOT NULL AND parentRel.subtreeTags IS NOT NULL
+                          THEN apoc.convert.fromJsonMap(parentRel.subtreeTags)
+                          ELSE {} END as parentTags
 
-            // Get the direct parent relationship for each child (the one closest to the child)
-            WITH currentNode, childNode, childRels,
-                 childRels[size(childRels)-1] as directChildRel
+                // Convert parent tags to inherited tags (true -> inherit, inherit -> inherit)
+                WITH currentNode, currentRel, currentTags, parentTags,
+                     apoc.map.fromPairs([key in keys(parentTags) WHERE parentTags[key] IN [true, "inherit"] | [key, "inherit"]]) as inheritedTags
 
-            // Get existing child tags and add inherited tag, preserving existing tags
-            WITH currentNode, childNode, directChildRel,
-                 CASE WHEN directChildRel.subtreeTags IS NOT NULL
-                      THEN apoc.convert.fromJsonMap(directChildRel.subtreeTags)
-                      ELSE {} END as existingChildTags
+                // Filter out inherited tags that are no longer present on parent, keep explicit tags
+                WITH currentNode, currentRel, currentTags, inheritedTags, parentTags,
+                     apoc.map.fromPairs([key in keys(currentTags) WHERE currentTags[key] = true OR (currentTags[key] = "inherit" AND key IN keys(parentTags) AND parentTags[key] IN [true, "inherit"]) | [key, currentTags[key]]]) as filteredCurrentTags
 
-            // Set the inherited tag on child nodes, preserving existing tags
-            SET directChildRel.subtreeTags = apoc.convert.toJson(existingChildTags)
+                // Merge inherited tags with filtered current tags
+                WITH currentNode, currentRel, filteredCurrentTags, inheritedTags,
+                     apoc.map.merge(inheritedTags, filteredCurrentTags) as updatedTags
 
-            RETURN count(*)',
-                [
-                    'aggregateId' => $nodeAggregateId->value,
-                    'contentStreamId' => $contentStreamId->value,
-                    'affectedDimensionSpacePointHashes' => $affectedDimensionSpacePointHashes,
-                ]
-            ));
+                // Update the relationship with proper inherited tags
+                SET currentRel.subtreeTags = CASE WHEN size(keys(updatedTags)) > 0
+                                                  THEN apoc.convert.toJson(updatedTags)
+                                                  ELSE null END
+
+                // Now handle all descendant nodes recursively
+                WITH currentNode
+                OPTIONAL MATCH (descendantNode)-[descendantRels:IS_CHILD*1.. {contentStreamId: $contentStreamId, dimensionSpacePointHash: $dimensionSpacePointHash}]->(currentNode)
+
+                // For each descendant, get its direct parent relationship
+                WITH descendantNode, descendantRels,
+                     descendantRels[0] as directDescendantRel
+
+                // Find the direct parent node and its relationship
+                WITH descendantNode, directDescendantRel,
+                     endNode(directDescendantRel) as directParentNode
+
+                OPTIONAL MATCH (directParentNode)-[directParentRel:IS_CHILD {contentStreamId: $contentStreamId, dimensionSpacePointHash: $dimensionSpacePointHash}]->()
+
+                // Get descendant current tags and parent tags
+                WITH descendantNode, directDescendantRel, directParentRel,
+                     CASE WHEN directDescendantRel.subtreeTags IS NOT NULL
+                          THEN apoc.convert.fromJsonMap(directDescendantRel.subtreeTags)
+                          ELSE {} END as descendantCurrentTags,
+                     CASE WHEN directParentRel IS NOT NULL AND directParentRel.subtreeTags IS NOT NULL
+                          THEN apoc.convert.fromJsonMap(directParentRel.subtreeTags)
+                          ELSE {} END as descendantParentTags
+
+                // Convert parent tags to inherited tags for descendant
+                WITH descendantNode, directDescendantRel, descendantCurrentTags, descendantParentTags,
+                     apoc.map.fromPairs([key in keys(descendantParentTags) WHERE descendantParentTags[key] IN [true, "inherit"] | [key, "inherit"]]) as descendantInheritedTags
+
+                // Filter out inherited tags that are no longer present on parent, keep explicit tags
+                WITH descendantNode, directDescendantRel, descendantCurrentTags, descendantInheritedTags, descendantParentTags,
+                     apoc.map.fromPairs([key in keys(descendantCurrentTags) WHERE descendantCurrentTags[key] = true OR (descendantCurrentTags[key] = "inherit" AND key IN keys(descendantParentTags) AND descendantParentTags[key] IN [true, "inherit"]) | [key, descendantCurrentTags[key]]]) as filteredDescendantCurrentTags
+
+                // Merge inherited tags with filtered current explicit tags
+                WITH descendantNode, directDescendantRel, filteredDescendantCurrentTags, descendantInheritedTags,
+                     apoc.map.merge(descendantInheritedTags, filteredDescendantCurrentTags) as descendantUpdatedTags
+
+                // Update descendant relationship with proper inherited tags
+                SET directDescendantRel.subtreeTags = CASE WHEN size(keys(descendantUpdatedTags)) > 0
+                                                           THEN apoc.convert.toJson(descendantUpdatedTags)
+                                                           ELSE null END
+
+                RETURN count(*)',
+                    [
+                        'aggregateId' => $nodeAggregateId->value,
+                        'contentStreamId' => $contentStreamId->value,
+                        'dimensionSpacePointHash' => $dimensionSpacePointHash,
+                    ]
+                )
+            );
+        }
     }
 }
